22018679b6add53e7b669a68dcbef1107889c514
james <jameskhor6@gmail.com>
2023-07-09 17:42:22 +0800

    Updated project to work with OTP requests + updated to realtime database

---
+++ app/src/main/java/com/gnj/e_koperasi/ForgotPassword.java
@@ -0,0 +1,100 @@
+package com.gnj.e_koperasi;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.concurrent.TimeUnit;
+
+public class ForgotPassword extends AppCompatActivity {
+    String PhoneNum;
+    EditText Phone;
+    Button otpbtn;
+    FirebaseAuth mAuth;
+    DatabaseReference userRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+
+        Phone = findViewById(R.id.etPhone);
+        otpbtn = findViewById(R.id.btnSend);
+        mAuth = FirebaseAuth.getInstance();
+        userRef = FirebaseDatabase.getInstance().getReference("user");
+
+        otpbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PhoneNum = "+60" + Phone.getText().toString().trim();
+                Query query = userRef.orderByChild("phone").equalTo(PhoneNum);
+                query.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists()) {
+                            DataSnapshot firstChild = dataSnapshot.getChildren().iterator().next();
+                            String id = firstChild.getKey();
+                            PhoneNum = firstChild.child("phone").getValue(String.class);
+
+                            otpbtn.setVisibility(View.INVISIBLE);
+
+                            PhoneAuthProvider.getInstance().verifyPhoneNumber(PhoneNum, 60, TimeUnit.SECONDS, ForgotPassword.this,
+                                    new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                                        @Override
+                                        public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                                            otpbtn.setVisibility(View.VISIBLE);
+                                        }
+
+                                        @Override
+                                        public void onVerificationFailed(@NonNull FirebaseException e) {
+                                            otpbtn.setVisibility(View.VISIBLE);
+                                            Toast.makeText(ForgotPassword.this, "Verification failed: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                        }
+
+                                        @Override
+                                        public void onCodeSent(@NonNull String otp, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                                            super.onCodeSent(otp, forceResendingToken);
+                                            otpbtn.setVisibility(View.VISIBLE);
+                                            Intent intent = new Intent(ForgotPassword.this, OTPActivity.class);
+                                            intent.putExtra("otp", otp);
+                                            intent.putExtra("mobile", PhoneNum);
+                                            startActivity(intent);
+                                        }
+                                    }
+                            );
+
+                        } else {
+                            Phone.setError("Phone number is not registered.");
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        // Handle onCancelled appropriately
+                    }
+                });
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/gnj/e_koperasi/MainActivity.java
@@ -0,0 +1,135 @@
+package com.gnj.e_koperasi;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnLogin;
+    TextView tvSignUp, tvGuest, tvForgotPassword;
+    EditText userID, password;
+    String id, pass;
+    DatabaseReference userRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnLogin = findViewById(R.id.btnLogin);
+        tvSignUp = findViewById(R.id.tvSignUp);
+        userID = findViewById(R.id.etUserID);
+        password = findViewById(R.id.etPassword);
+        tvForgotPassword = findViewById(R.id.tvForgotPassword);
+        tvGuest = findViewById(R.id.tvGuest);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        userRef = database.getReference("user");
+
+        tvSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent reg = new Intent(getApplicationContext(), Register.class);
+                startActivity(reg);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                id = (userID.getText().toString()).toUpperCase().trim();
+                pass = password.getText().toString();
+
+                if (isValid()) {
+                    Query query = userRef.orderByChild("customer_id").equalTo(id);
+                    query.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            if (dataSnapshot.exists()) {
+                                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                                    User user = snapshot.getValue(User.class);
+
+                                    if (user != null) {
+                                        String bcryptHashString = user.getPassword();
+                                        BCrypt.Result result = BCrypt.verifyer().verify(pass.toCharArray(), bcryptHashString);
+
+                                        if (result.verified) {
+                                            Toast.makeText(getApplicationContext(), "Welcome, " + user.getName() + "!", Toast.LENGTH_SHORT).show();
+                                            Intent main = new Intent(getApplicationContext(), ScanItems.class);
+                                            Bundle bundle = new Bundle();
+                                            bundle.putString("id", id);
+                                            main.putExtras(bundle);
+                                            startActivity(main);
+                                            return;
+                                        }
+                                    }
+                                }
+                                password.setError("Incorrect username or password.");
+                            } else {
+                                userID.setError("Account not registered. Please register an account.");
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                            Toast.makeText(getApplicationContext(), "Error: " + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        });
+
+        tvGuest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent guest = new Intent(getApplicationContext(), ScanItems.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("id", "GUEST");
+                guest.putExtras(bundle);
+                startActivity(guest);
+            }
+        });
+
+        tvForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent forgot = new Intent(getApplicationContext(), ForgotPassword.class);
+                startActivity(forgot);
+            }
+        });
+    }
+
+    public boolean isValid() {
+        if (id.isEmpty() && pass.isEmpty()) {
+            userID.setError("Enter your matric number or staff ID.");
+            password.setError("Enter your password.");
+            return false;
+        } else if (id.isEmpty()) {
+            userID.setError("Enter your matric number or staff ID.");
+            return false;
+        } else if (pass.isEmpty()) {
+            password.setError("Enter your password.");
+            return false;
+        } else if (pass.length() < 8) {
+            password.setError("Password must be at least 8 characters long.");
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
