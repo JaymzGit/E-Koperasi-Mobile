7af2b38a5f3ff40769f81770a52953f67843a4bf
NaqibKhusairy <naaqibkhusairy@gmail.com>
2023-07-21 02:04:23 +0800

    add function to show check out

--- app/src/main/java/com/gnj/e_koperasi/ShowCheckOut.java
+++ app/src/main/java/com/gnj/e_koperasi/ShowCheckOut.java
@@ -3,9 +3,11 @@ package com.gnj.e_koperasi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ShowCheckOut extends AppCompatActivity {
     String id,itemName,itemPrice,quantity,totalPrice;
+    TextView ItemName,ItemPrice,Quantity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +19,12 @@ public class ShowCheckOut extends AppCompatActivity {
         itemPrice=bundle.getString("itemPrice");
         quantity=bundle.getString("quantity");
         totalPrice=bundle.getString("totalPrice");
+        ItemName=findViewById(R.id.ItemName);
+        ItemPrice=findViewById(R.id.ItemPrice);
+        Quantity=findViewById(R.id.Quantity);
+
+        ItemName.setText(itemName);
+        ItemPrice.setText(itemPrice);
+        Quantity.setText(quantity);
     }
 }
